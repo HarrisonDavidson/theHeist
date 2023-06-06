9b708cdd8b280054c658128219228fe7108e782a
Harrison Davidson <125293541+HarrisonDavidson@users.noreply.github.com>
2023-06-06 10:40:54 -0400

    Part one

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string? teammateName;
+        int? teammateSkill;
+        double? teammateCourage;
+
+        Console.WriteLine("Enter your teammate's name:");
+        teammateName = Console.ReadLine();
+
+        Console.WriteLine("Enter your teammate's skill level:");
+        teammateSkill = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter your teammate's courage factor:");
+        teammateCourage = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Name: " + teammateName);
+        Console.WriteLine("Skill level:" + teammateSkill);
+        Console.WriteLine("Courage factor:" + teammateCourage);
+    }
+}
